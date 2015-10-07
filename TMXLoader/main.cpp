c1bc91bae0458d5be047c3b8a65e9aebba6d2e39
Martin Grant <martingrant@outlook.com>
2015-10-07 14:40:11 +0100

    Removed globals and added function arguments and delete calls for SDL and resources.

--- TMXLoader/main.cpp
+++ TMXLoader/main.cpp
@@ -14,15 +14,6 @@
 #include "TMXLoader.h"
 #include "Levels/TMXMap.h"
 
-
-SDL_Window* window;
-SDL_Renderer* renderer;
-
-SDL_Texture* tex1;
-SDL_Texture* spriteSheet;
-
-TMXLoader* loader;
-
 bool update(SDL_Event &events)
 {
     while (SDL_PollEvent(&events))
@@ -42,32 +33,31 @@ bool update(SDL_Event &events)
                 break;
         }
     }
-    
     return true;
 }
 
 
-void renderRectOutline(SDL_Rect& rect, Uint8 red, Uint8 green, Uint8 blue)
+void renderRectOutline(SDL_Renderer* renderer, SDL_Rect& rect, Uint8 red, Uint8 green, Uint8 blue)
 {
     SDL_SetRenderDrawColor(renderer, red, green, blue, SDL_ALPHA_TRANSPARENT);
     SDL_RenderDrawRect(renderer, &rect);
 }
 
 
-void renderRectFilled(SDL_Rect& rect, Uint8 red, Uint8 green, Uint8 blue)
+void renderRectFilled(SDL_Renderer* renderer, SDL_Rect& rect, Uint8 red, Uint8 green, Uint8 blue)
 {
     SDL_SetRenderDrawColor(renderer, red, green, blue, SDL_ALPHA_TRANSPARENT);
     SDL_RenderFillRect(renderer, &rect);
 }
 
 
-void renderTexture(SDL_Texture* texture, SDL_Rect& destRect)
+void renderTexture(SDL_Renderer* renderer, SDL_Texture* texture, SDL_Rect& destRect)
 {
     SDL_RenderCopy(renderer, texture, NULL, &destRect);
 }
 
 
-void renderSpriteSheet()
+void renderSpriteSheet(SDL_Renderer* renderer, SDL_Texture* texture)
 {
     int tileWidth = 25;
     int sheetWidth = 75;
@@ -78,70 +68,43 @@ void renderSpriteSheet()
         SDL_Rect srcrect = { tileWidth * i, 0, 25, 25 };
         SDL_Rect dstrect = { i * 100, 100, 25, 25 };
         
-        SDL_RenderCopy(renderer, spriteSheet, &srcrect, &dstrect);
+        SDL_RenderCopy(renderer, texture, &srcrect, &dstrect);
     }
 }
 
 
-void testRenderMap()
+void testRenderMap(SDL_Renderer* renderer, SDL_Texture* texture, TMXLoader* loader)
 {
     char tileID = 0;
     
-    int width = 25;
-    int height = 25;
+    int tileWidth = loader->getMap().getTileWidth();
+    int tileHeight = loader->getMap().getTileHeight();
 
-    int row = 0;
-    int column = 0;
-    
-    for (int i = 0; i < 10; ++i)
+    for (int i = 0; i < loader->getMap().getWidth(); ++i)
     {
-        for (int j = 0; j < 10; ++j)
+        for (int j = 0; j < loader->getMap().getHeight(); ++j)
         {
             // get the tile at current position
-            //tileID = map[i][j];
             tileID = loader->getMap().getLayer().getTileLayer()[0][i][j];
             
             // only render if it is an actual tile (1, 2 or 3)
             if (tileID > 0)
             {
-                SDL_Rect srcrect = { (tileID - 1) * 25, 0, width, height };
-                SDL_Rect dstrect = { column, row, width, height };
-                SDL_RenderCopy(renderer, spriteSheet, &srcrect, &dstrect);
+                SDL_Rect srcrect = { (tileID - 1) * 25, 0, tileWidth, tileHeight };
+                SDL_Rect dstrect = { j * 25, i * 25, tileWidth, tileHeight };
+                SDL_RenderCopy(renderer, texture, &srcrect, &dstrect);
             }
-            column += 25;
         }
-        row += 25;
-        column = 0;
     }
 }
 
 
-void render()
+void render(SDL_Renderer* renderer, SDL_Texture* texture, TMXLoader* loader)
 {
     SDL_SetRenderDrawColor(renderer, 10, 255, 255, SDL_ALPHA_TRANSPARENT);
     SDL_RenderClear(renderer);
-
-    SDL_Rect rect;
-    rect.x = 50;
-    rect.y = 50;
-    rect.w = 100;
-    rect.h = 100;
     
-    //renderRectOutline(rect, 255, 50, 50);
-    
-    SDL_Rect rect2;
-    rect2.x = 200;
-    rect2.y = 50;
-    rect2.w = 100;
-    rect2.h = 100;
-    
-    //renderRectFilled(rect2, 50, 50, 255);
-    
-    //renderTexture(tex1, rect2);
-    
-    //renderSpriteSheet();
-    
-    testRenderMap();
+    testRenderMap(renderer, texture, loader);
     
     SDL_RenderPresent(renderer);
 }
@@ -149,39 +112,40 @@ void render()
 
 int main(int argc, const char * argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cout << "SDL_Init: " << SDL_GetError() << std::endl;
+    }
     
     Uint32 windowFlags = SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN;
-    window = SDL_CreateWindow("TMXLoader", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, windowFlags);
-    renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED);
     
-    SDL_SetRenderDrawColor(renderer, 10, 255, 255, SDL_ALPHA_TRANSPARENT);
+    SDL_Window* window = SDL_CreateWindow("TMXLoader", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, windowFlags);
     
-    tex1 = IMG_LoadTexture(renderer, "Assets/button.png");
+    if (window == NULL)
+    {
+        std::cout << "SDL_CreateWindow: " << SDL_GetError() << std::endl;
+    }
     
-    spriteSheet = IMG_LoadTexture(renderer, "Assets/spritesheet1.png");
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED);
     
+    SDL_Texture* spriteSheet = IMG_LoadTexture(renderer, "Assets/spritesheet1.png");
     
-
-    
-    
-    loader = new TMXLoader();
-    
+    TMXLoader* loader = new TMXLoader();
     loader->loadLevel("Assets/testlevel.tmx");
-    
-    
-    
-    
-    
-    
     
     bool running = true;
     SDL_Event events;
     while (running == true)
     {
         running = update(events);
-        render();
+        render(renderer, spriteSheet, loader);
     }
+    
+    delete loader;
+    
+    SDL_DestroyTexture(spriteSheet);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     
     return 0;
 }
